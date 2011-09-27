03cce200e3e64cc840b3f56f051fea489bff5e64
Garth Williams <garth@wi.llia.ms>
2011-09-27 18:12:49 +0000

    Pending messages now show nicely

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(process.env.C9_PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 // web sockets
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,83 +1,89 @@
 //create the app
 Chat = SC.Application.create();
 
-Chat.LocationButtonView = SC.Button.extend({
+//pending messages
+Chat.pending = SC.ArrayProxy.create({
+  // Initialize the array controller with an empty array.
+  content: [],
+
+  addPerson: function(person) {
+    if (!this.contains(person)) {
+      this.pushObject(person);
+    }
+  },
+
+  removePerson: function(person) {
+    this.removeObject(person);
+  }
+});
+
+//send geo location
+Chat.locationButton = SC.Button.extend({
   click: function() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        socket.emit('message', { person: Chat.person.name, message: "is at " + position.coords.latitude + ' ' + position.coords.longitude });
+        Chat.socket.emit('message', {
+          person: Chat.person.name,
+          message: "is at " + position.coords.latitude + ' ' +
+                   position.coords.longitude
+        });
       });
     }
   }
 });
+
+//current person
 Chat.person = SC.Object.create({"name":null});
-Chat.PersonView = SC.TextField.extend({
+Chat.personView = SC.TextField.extend({
   valueBinding: "Chat.person.name"
 });
-Chat.AddMessageView = SC.TextField.extend({
+
+//prepare message
+Chat.Message = SC.Object.extend({"time": null, "person": null, "message": null});
+Chat.messageView = SC.TextField.extend({
   keyUp: function(event) {
-    if (event.which != 13) //<cr>
-    {
-      //notify that a message is coming
-      socket.emit('typing', { person: Chat.person.name });
-    }
-    else
-    {
-      //send the message
-      var value = this.get('value');
-      if (value) {
-        socket.emit('message', { person: Chat.person.name, message: value });
+    var value = $('#message').val();
+//    var value = this.get('value'); //this value is not always set (bug in SC?)
+    if (value) {
+      if (event.which != 13) { //not <cr>
+          //notify that a message is coming
+          Chat.socket.emit('typing', { person: Chat.person.name });
+      }
+      else {
+        //send the message
+        Chat.socket.emit('message', {
+          person: Chat.person.name,
+          message: value
+        });
         this.set('value', '');
       }
     }
   }
 });
 
-//define the message object
-Chat.Message = SC.Object.extend({"time": null, "person": null, "message": null});
-
 //create a message controller
-Chat.messagesController = SC.ArrayProxy.create({
+Chat.messages = SC.ArrayProxy.create({
   // Initialize the array controller with an empty array.
   content: [],
 
-  createMessage: function(data) {
-    this.removeComing(data.person);
-    this.insertAt(0, Chat.Message.create(data));
-  },
-  
-  removeComing: function(person)
-  {
-    for (var i = 0; i < this.length; i++)
-    {
-      console.log(i)
-      while (this[i].person == person && this[i].message == '[is typing...]')
-      {
-        console.log('removing');
-        this.removeAt(i);
-      }
-    }
-  },
-  
-  coming: function(data) {
-    this.removeComing(data.person);
-    data.message = '[is typing...]';
+  addMessage: function(data) {
+    Chat.pending.removePerson(data.person);
     this.insertAt(0, Chat.Message.create(data));
   }
 });
 
-var socket;
+//wire up socket.io
 $(function() {
   //connect the socket
-  socket = io.connect('http://localhost:3000');
+  Chat.socket = io.connect('/');
   
   //receive message
-  socket.on('message', function (data) {
-    Chat.messagesController.createMessage(data);
+  Chat.socket.on('message', function (data) {
+    Chat.messages.addMessage(data);
   });
   
   //reveive a typing message
-  socket.on('typing', function (data){
-    Chat.messagesController.coming(data);
+  Chat.socket.on('typing', function (data){
+    Chat.pending.addPerson(data.person);
   });
 });
