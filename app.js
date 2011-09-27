40c76ddd5cb79bdda1e9b8f21a430f1f2e264349
Garth Williams <garth@wi.llia.ms>
2011-09-27 18:39:42 +0000

    Tidied up the server and moved chat.html to index.html

--- app.js
+++ app.js
@@ -1,54 +1,26 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-
 var app = module.exports = express.createServer();
-
 var io = require('socket.io').listen(app);
 
-// Configuration
-
+//configure express
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
-});
-
+//start the http server
 app.listen(process.env.C9_PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-// web sockets
-
+//setup the chat server
 var clients = [];
 
 io.sockets.on('connection', function (socket) {
-  //add the new client
+  //add the new client and send welcome message
   clients.push(socket);
-  //sent welcome message
   socket.emit('message', { time: new Date().toLocaleTimeString(), person: 'Chat Server', message: 'Welcome to Chat!' });
-  //wait for messages
+
+  //relay messages
   socket.on('message', function (data) {
     //add server time to the message
     data.time = new Date().toLocaleTimeString();
@@ -60,11 +32,11 @@ io.sockets.on('connection', function (socket) {
       clients[i].emit('message', data);
     }
   });
+
+  //notify pending messages
   socket.on('typing', function (data) {
-    //add server time to the message
-    data.time = new Date().toLocaleTimeString();
     //log the message
-    console.log(data.person + ': is typeing');
+    console.log(data.person + ': is typing');
     //send messages to all clients
     for (var i = 0; i < clients.length; i++)
     {
